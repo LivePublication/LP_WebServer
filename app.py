d20556caa847e0ae894015529fd62115e4bede56
Nelis Drost <nelis.drost@gmail.com>
2023-06-09 16:42:50 +1200

    Basic implementation of papers list page and individual paper page

--- app.py
+++ app.py
@@ -6,7 +6,29 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='Home', text='This is the home page')
+    return render_template('index.html', title='Live Publications', text='This is the home page')
+
+
+papers_list = [
+    {'title': f'Paper {i}', 'authors': 'Author 1, Author 2', 'year': '2020',
+     'link': f'papers/{i}'}
+    for i in range(5)
+]
+
+
+@app.route('/papers')
+def papers():
+    # TODO: Build list of papers available
+    return render_template('papers.html',
+                           title='Live Publications',
+                           papers=papers_list)
+
+
+@app.route('/papers/<int:paper_id>')
+def paper(paper_id):
+    return render_template('paper.html',
+                           title='Live Publications',
+                           paper=papers_list[paper_id])
 
 
 if __name__ == '__main__':
